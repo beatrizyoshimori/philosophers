85cc9d05db20ad51b94ce0c51de7e3d93857896b
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-06-28 21:35:20 -0300

    added new functions

--- bonus/includes_bonus/philo_bonus.h
+++ bonus/includes_bonus/philo_bonus.h
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 19:02:58 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/06/26 21:14:27 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/06/28 20:03:13 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,23 @@
 
 # include <stdio.h>
 # include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/time.h>
 # include <unistd.h>
+# include <fcntl.h>
+# include <semaphore.h>
+# include <stdlib.h>
 
 typedef struct s_data
 {
-	int				num_philo;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				num_times_to_eat;
-	int				dinner_is_over;
-	time_t			start_time;
-	// pthread_mutex_t	*forks;
+	int		num_philo;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		num_times_to_eat;
+	int		dinner_is_over;
+	time_t	start_time;
+	sem_t	*forks;
 	// pthread_mutex_t	printf_mutex;
 	// pthread_mutex_t	meal_mutex;
 	// pthread_mutex_t	dinner_mutex;
@@ -43,5 +48,24 @@ typedef struct s_philo
 	// pthread_mutex_t	*left_fork;
 	// pthread_mutex_t	*right_fork;
 }	t_philo;
+
+int		get_meals(t_philo *philo);
+void	set_meals(t_philo *philo);
+int		get_last_meal(t_philo *philo);
+void	set_last_meal(t_philo *philo);
+
+void	*dinner(t_philo *philo);
+
+int		dinner_is_over(t_philo *philo);
+void	print_state(t_philo *philo, char *state);
+int		ft_atoi(const char *nptr);
+
+long	timestamp(void);
+long	timenow(long firststamp);
+
+// void	*monitoring(void *ptr);
+
+void	init_philos(t_data *data, t_philo **philo);
+void	get_data(int argc, char *argv[], t_data **data);
 
 #endif
