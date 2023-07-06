899823c4df864989f9c9e4c800eabf02e9f258a3
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-07-05 21:58:59 -0300

    added check_if_died function

--- bonus/sources_bonus/dinner_bonus.c
+++ bonus/sources_bonus/dinner_bonus.c
@@ -6,7 +6,7 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 18:55:22 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/07/04 21:53:20 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/07/05 21:55:58 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,43 @@ static void	philo_died(t_philo *philo)
 	exit(1);
 }
 
+void	check_if_died(t_philo *philo, long time)
+{
+	long	start_time;
+	long	current_time;
+
+	start_time = timestamp();
+	while (timestamp() - start_time < time)
+	{
+		usleep(10);
+		current_time = timenow(philo->data->start_time);
+		if ((current_time - philo->time_since_last_meal) > philo->data->time_to_die)
+		{
+			sem_wait(philo->data->print_sem);
+			current_time = timenow(philo->data->start_time);
+			printf("%ld %d died\n", current_time, philo->id);
+			free_all(philo);
+			exit(1);
+		}
+	}
+}
+
 static void	eat(t_philo **philo)
 {
-	long	curr_time;
+	// long	curr_time;
 
 	sem_wait((*philo)->data->forks);
 	sem_wait((*philo)->data->forks);
 	print_state(*philo, "has taken a fork");
 	print_state(*philo, "has taken a fork");
 	print_state(*philo, "is eating");
-	curr_time = timenow((*philo)->data->start_time);
-	if (curr_time + (*philo)->data->time_to_eat
-		> (*philo)->data->time_to_die + (*philo)->time_since_last_meal)
-		philo_died(*philo);
-	(*philo)->time_since_last_meal = curr_time;
-	usleep((*philo)->data->time_to_eat * 1000);
+	// curr_time = timenow((*philo)->data->start_time);
+	// if (curr_time + (*philo)->data->time_to_eat
+	// 	> (*philo)->data->time_to_die + (*philo)->time_since_last_meal)
+	// 	philo_died(*philo);
+	(*philo)->time_since_last_meal = timenow((*philo)->data->start_time);
+	// usleep((*philo)->data->time_to_eat * 1000);
+	check_if_died(*philo, (*philo)->data->time_to_eat);
 	sem_post((*philo)->data->forks);
 	sem_post((*philo)->data->forks);
 	(*philo)->num_meals++;
@@ -46,14 +68,15 @@ static void	eat(t_philo **philo)
 
 static void	sleep_and_think(t_philo *philo)
 {
-	long	curr_time;
+	// long	curr_time;
 
-	curr_time = timenow(philo->data->start_time);
+	// curr_time = timenow(philo->data->start_time);
 	print_state(philo, "is sleeping");
-	if (curr_time + philo->data->time_to_eat
-		> philo->data->time_to_die + philo->time_since_last_meal)
-		philo_died(philo);
-	usleep(philo->data->time_to_sleep * 1000);
+	// if (curr_time + philo->data->time_to_sleep
+	// 	> philo->data->time_to_die + philo->time_since_last_meal)
+	// 	philo_died(philo);
+	// usleep(philo->data->time_to_sleep * 1000);
+	check_if_died(philo, philo->data->time_to_sleep);
 	print_state(philo, "is thinking");
 	usleep(500);
 }
