e15d0c630e84115cd713af552788dab5878968e3
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-07-10 18:48:59 -0300

    updated free_all function

--- bonus/sources_bonus/philo_utils_bonus.c
+++ bonus/sources_bonus/philo_utils_bonus.c
@@ -6,21 +6,21 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 19:00:06 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/07/04 21:44:07 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/07/10 17:51:26 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
-void	free_all(t_philo *philo)
+void	free_all(t_data *data)
 {
-	sem_close(philo->data->forks);
-	sem_close(philo->data->print_sem);
+	sem_close(data->forks);
+	sem_close(data->print_sem);
 	sem_unlink("forks_sem");
 	sem_unlink("print_sem");
-	free(philo->data->pid);
-	free(philo->data);
-	free(philo);
+	free(data->pid);
+	free(data->philos);
+	free(data);
 }
 
 void	print_state(t_philo *philo, char *state)
