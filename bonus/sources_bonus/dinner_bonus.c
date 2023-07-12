474a603def3125f1dd771734f7539b855a1f175e
beatrizyoshimori <103694028+beatrizyoshimori@users.noreply.github.com>
2023-07-12 21:38:48 +0000

    added functions to dinner_utils_bonus.c

--- bonus/sources_bonus/dinner_bonus.c
+++ bonus/sources_bonus/dinner_bonus.c
@@ -12,67 +12,18 @@
 
 #include "philo_bonus.h"
 
-static void	check_if_died(t_data *data)
-{
-	long	curr_time;
-
-	curr_time = timenow(data->start_time);
-	if ((curr_time - data->philos[data->curr_philo].time_since_last_meal)
-		> data->time_to_die)
-	{
-		sem_wait(data->print_sem);
-		curr_time = timenow(data->start_time);
-		printf("%ld %d died\n", curr_time,
-			data->philos[data->curr_philo].id);
-		free_all(data);
-		exit(1);
-	}
-}
-
-static void	wait_for_fork(t_data *data)
-{
-	while (*(long *)(data->philos[data->curr_philo].forks) < 2)
-	{
-		printf("AQUI\n");
-		usleep(10);
-		check_if_died(data);
-	}
-}
-
-static void	action_time(t_data *data, long time)
-{
-	long	start_time;
-
-	start_time = timestamp();
-	while (timestamp() - start_time < time)
-	{
-		usleep(50);
-		check_if_died(data);
-	}
-}
-
 static void	eat(t_data **data)
 {
-	sem_wait((*data)->philos[(*data)->curr_philo].left_fork);
-	sem_wait((*data)->philos[(*data)->curr_philo].right_fork);
-	// if (((timenow((*data)->start_time)) - (*data)->philos[(*data)->curr_philo].time_since_last_meal)
-	// 		> (*data)->time_to_die)
-	// {
-	// 	sem_wait((*data)->print_sem);
-	// 	long curr_time = (*data)->philos[(*data)->curr_philo].time_since_last_meal + (*data)->time_to_die;
-	// 	printf("%ld %d died\n", curr_time,
-	// 		(*data)->philos[(*data)->curr_philo].id);
-	// 	free_all(*data);
-	// 	exit(1);
-	// }
+	sem_wait((*data)->forks);
+	sem_wait((*data)->forks);
 	print_state(&(*data)->philos[(*data)->curr_philo], "has taken a fork");
 	print_state(&(*data)->philos[(*data)->curr_philo], "has taken a fork");
 	print_state(&(*data)->philos[(*data)->curr_philo], "is eating");
 	(*data)->philos[(*data)->curr_philo].time_since_last_meal
 		= timenow((*data)->start_time);
 	action_time(*data, (*data)->time_to_eat);
-	sem_post((*data)->philos[(*data)->curr_philo].left_fork);
-	sem_post((*data)->philos[(*data)->curr_philo].right_fork);
+	sem_post((*data)->forks);
+	sem_post((*data)->forks);
 	(*data)->philos[(*data)->curr_philo].num_meals++;
 }
 
