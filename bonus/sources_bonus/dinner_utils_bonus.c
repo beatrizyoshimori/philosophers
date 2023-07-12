5eac1f6aa87ab26ac6ae48bf42755bea63c07ce8
beatrizyoshimori <103694028+beatrizyoshimori@users.noreply.github.com>
2023-07-12 21:36:54 +0000

    created dinner_utils_bonus.c file and added functions

---
+++ bonus/sources_bonus/dinner_utils_bonus.c
@@ -0,0 +1,39 @@
+#include "philo_bonus.h"
+
+void	check_if_died(t_data *data)
+{
+	long	curr_time;
+
+	curr_time = timenow(data->start_time);
+	if ((curr_time - data->philos[data->curr_philo].time_since_last_meal)
+		> data->time_to_die)
+	{
+		sem_wait(data->print_sem);
+		curr_time = timenow(data->start_time);
+		printf("%ld %d died\n", curr_time,
+			data->philos[data->curr_philo].id);
+		free_all(data);
+		exit(1);
+	}
+}
+
+void	wait_for_fork(t_data *data)
+{
+	while (*(long *)(data->forks) < 2)
+	{
+		usleep(10);
+		check_if_died(data);
+	}
+}
+
+void	action_time(t_data *data, long time)
+{
+	long	start_time;
+
+	start_time = timestamp();
+	while (timestamp() - start_time < time)
+	{
+		usleep(10);
+		check_if_died(data);
+	}
+}
