7a5e8d6bf248719c34b3a6e00c179d833576281e
beatrizyoshimori <beatrizyoshimori@gmail.com>
2023-07-14 21:47:55 -0300

    removed check_args function

--- philo/sources/philo.c
+++ philo/sources/philo.c
@@ -6,29 +6,18 @@
 /*   By: byoshimo <byoshimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 14:47:51 by byoshimo          #+#    #+#             */
-/*   Updated: 2023/06/26 20:43:31 by byoshimo         ###   ########.fr       */
+/*   Updated: 2023/07/14 21:20:20 by byoshimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-static void	check_args(int argc)
-{
-	if (argc < 5 || argc > 6)
-	{
-		printf("Wrong arguments!\nUsage:\n./philo number_of_philosophers");
-		printf(" time_to_die time_to_eat time_to_sleep ");
-		printf("[number_of_times_each_philosopher_must_eat]\n");
-		exit(1);
-	}
-}
 
 int	main(int argc, char *argv[])
 {
 	t_data		*data;
 	t_philo		*philo;
 
-	check_args(argc);
+	check_args(argc, argv);
 	get_data(argc, argv, &data);
 	init_philos(data, &philo);
 	mutexes_init(data);
